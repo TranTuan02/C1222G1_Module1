2f2f1283e4165a7d381e1f5b84aaed6d056bb940
unknown <you@example.com>
2023-01-10 19:15:05 +0700

    update

--- modue1/ss8_cau_truc_dieu_kien/chuyen_doi_tien_te.js
+++ modue1/ss8_cau_truc_dieu_kien/chuyen_doi_tien_te.js
@@ -1,16 +1,43 @@
-function Currency() {
+function intermediate(from, to) {
+    switch (from) {
+        case "VND":
+            return to/ 23000;
+        case "USD":
+            return to;
+        case "JPY":
+            return 0.0076 * to;
+        case "RMB":
+            return 0.15 * to;
+        case "GBP":
+            return 1.21 * to;
+
+    }
+
+}
+
+function fromIntermediateTo(from, to) {
+    switch (from) {
+        case "VND":
+            return to * 23000;
+        case "USD":
+            return to;
+        case "JPY":
+            return to/ 0.0076;
+        case "RMB":
+            return to / 0.15;
+        case "GBP":
+            return to/ 1.21;
+
+
+    }
+
+}
+
+function result() {
+    let showResult = document.getElementById("showResult");
     let fromCurrency = document.getElementById("from").value;
     let toCurrency = document.getElementById("to").value;
     let amount = document.getElementById("amount").value;
-    let result;
-    if (fromCurrency === "USD" && toCurrency === "VND") {
-        result = "result" + (amount * 23000) + "VND";
-    } else if (fromCurrency === "USD" && toCurrency === "JPY") {
-        result = "result" + (amount * 133.95) + "JPY";
-    } else if (fromCurrency === "USD" && toCurrency === "RMB") {
-        result = "result" + (amount * 6.68) + "RMB";
-    } else if (fromCurrency === "USD" && toCurrency === "GBP") {
-        result = "result" + (amount * 0.84) + "GBP";
-    }
-    document.getElementById("Money").innerText = result;
+    let result = fromIntermediateTo(toCurrency, intermediate(fromCurrency, amount));
+    showResult.innerHTML = result;
 }
